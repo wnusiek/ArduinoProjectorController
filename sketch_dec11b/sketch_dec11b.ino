73dd38b1123249c3704a375ccb097b6b82d021e8
wnusiek <pawel.jan.wnuk@gmail.com>
2024-03-20 10:50:28 +0100

    added delay

--- sketch_dec11b/sketch_dec11b.ino
+++ sketch_dec11b/sketch_dec11b.ino
@@ -1,22 +1,15 @@
 void setup() {
   Serial.begin(9600);
+  
 /*
-
-Diody       outputPins
-zielona     8
-czerwona    9
-żółta       10
-niebieska   11
-biała       12
-
-Przyciski   inputPins
-Power       3
-Computer 1  4
-Computer 2  5
-HDMI        6
-Video       7
-
+Przyciski   inputPins  Diody       outputPins
+Power       3          zielona     8
+Computer 1  4          czerwona    9
+Computer 2  5          żółta       10
+HDMI        6          niebieska   11
+Video       7          biała       12
 */
+
   int outputPins [] = {8,9,10,11,12};
   for (int i = 0; i < sizeof(outputPins)/sizeof(outputPins[0]); i++){
     pinMode(outputPins[i], OUTPUT);     //USTAWIENIE WYJŚĆ SYGNALIZACYJNYCH
@@ -27,7 +20,7 @@ Video       7
   for (int i = 0; i < sizeof(inputPins)/sizeof(inputPins[0]); i++){
     pinMode(inputPins[i], INPUT_PULLUP);    //USTAWIENIE PRZYCISKÓW
   }
-
+  
 }
 
 class Button {
@@ -46,41 +39,43 @@ public:
   }
 
   void checkIfButtonPressed() {
-
     if (digitalRead(this->input) == LOW) {
       digitalWrite(this->output, HIGH);
       Serial.println(this->projectorCommand);
-      delay(200);
+      delay(300);
     } else {
       digitalWrite(this->output, LOW);
+      delay(10);
     }
   };
 
 };
-
-  boolean isOn = false;
+  boolean isOn = true;
 
   String returnPowerCommand() {
+    String command = "";
     if (isOn == true) {
+      command = "PWR OFF\r\n";
       isOn = false;
-      return "PWR OFF\r\n";
     } 
-    if (isOn == false) {
+    else {
+      command = "PWR ON\r\n";
       isOn = true;
-      return "PWR ON\r\n";
     }
+    return command;
   };
 
+
+void loop() {
+  
   Button power("Power", 3, returnPowerCommand(), 8);
   Button computer1("Computer 1 D-SUB VGA", 4, "SOURCE 11\r\n", 9);
   Button computer2("Computer 2 D-SUB VGA", 5, "SOURCE 21\r\n", 10);
   Button hdmi("HDMI", 6, "SOURCE 30\r\n", 11);
   Button video("Video", 7, "SOURCE 41\r\n", 12);
   Button buttons[] = {power, computer1, computer2, hdmi, video};
-
-void loop() {
-
   for (int i = 0; i < sizeof(buttons)/sizeof(buttons[0]); i++) {
+    
     buttons[i].checkIfButtonPressed();
   }
 
